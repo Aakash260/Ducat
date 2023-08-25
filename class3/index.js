dcb159bb4ee86144a6ecf8396cb140fb533d81b4
akash <akashnirwan26@gmail.com>
2023-08-25 13:37:05 +0530

    class4

---
+++ class3/index.js
@@ -0,0 +1,15 @@
+console.clear();//ise phle ke sare console clear
+console.time('Timer')
+console.log('log')
+console.error('error')
+console.warn('warn')
+console.debug('debug')
+console.info('info')
+console.timeEnd('Timer')
+console.log('Aakash "nirwan"')
+console.log("Akash 'nirwan'")
+
+
+console.log('semi-col importance;')  
+console.log('its')
+console.log('ok')
---
+++ class4/index.js
@@ -0,0 +1,27 @@
+// var a=10;
+// console.log("🚀 ~ file: index.js:2 ~ a:", a);
+//= means assignment
+
+// let b=10;
+// console.log("🚀 ~ file: index.js:6 ~ a:", a);
+
+// const c='2135';
+//const is not a variable 
+// console.log("🚀 ~ file: index.js:9 ~ c:", c)
+
+//compile check code as a whole ex-c++
+
+//interpreter check code line by line ex "viv  its show error before compile ex-python js
+
+//var can be redeclared
+
+// var d=10;
+// var d=20;
+// console.log("🚀 ~ file: index.js:20 ~ d:", d)
+
+// b=10 //let
+
+// Naming convention
+// camelCase
+// snake_case
+
