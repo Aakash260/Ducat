2810997aaecf8d545215449d68c969518e05d9d0
akash <akashnirwan26@gmail.com>
2023-09-04 17:25:55 +0530

    class6

---
+++ class6/index.js
@@ -0,0 +1,14 @@
+//number to string
+var a=10
+console.log(typeof a.toString(a))
+console.log(typeof String(a))
+//string to number
+var a='1'
+console.log(+a)
+console.log(Number(a))
+var a='akash'
+var b='nirwan'
+//concatination
+console.log(a+" "+b)
+//template String
+console.log(`${a}${b}`)
