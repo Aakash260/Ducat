9be4c7cecca9eb80fb58252716bacd57a870c350
akash <akashnirwan26@gmail.com>
2023-09-05 13:39:02 +0530

    class7

--- class6/index.js
+++ class6/index.js
@@ -1,14 +1,33 @@
-//number to string
-var a=10
-console.log(typeof a.toString(a))
-console.log(typeof String(a))
-//string to number
-var a='1'
-console.log(+a)
-console.log(Number(a))
-var a='akash'
-var b='nirwan'
-//concatination
-console.log(a+" "+b)
-//template String
-console.log(`${a}${b}`)
+// //number to string
+// var a=10
+// console.log(typeof a.toString(a))
+// console.log(typeof String(a))
+// //string to number
+// var a='1'
+// console.log(+a)
+// console.log(Number(a))
+// var a='akash'
+// var b='nirwan'
+// //concatination
+// console.log(a+" "+b)
+// //template String
+// console.log(`${a}${b}`)
+
+
+
+//Primitive Type (value Type)
+// number,string,undefined,Boolean,null, bigInt
+let a=2;
+console.log(typeof a)
+let b='hi'
+console.log(typeof b)
+let c;
+console.log(typeof c)
+let d=true;
+console.log(typeof d)
+console.log(Number.MAX_SAFE_INTEGER)
+let e=989105214678945612n//greater than Number.MAX_SAFE_INTEGER
+console.log(typeof e) //remmeber,we can't perform mix number with it.
+console.log(e)
+let f=null;
+console.log(f)
---
+++ class7/index.js
@@ -0,0 +1,13 @@
+//Non-premitive type (reference type)
+const a=[1,'javaScript','react',123,true]
+//js allow multi datatype in single array
+console.log(typeof a)
+console.log(Array.isArray(a))
+const b={
+    name:'akash',//also writen as 'name':'akash',
+    course:"javascript",
+    email:'akash@gmail.com'
+}
+console.log(b)
+
+
