827d4f4c10f14a2e88cd4daa7d84a011f8498c44
akash <akashnirwan26@gmail.com>
2023-09-06 20:43:29 +0530

    class8

---
+++ class8/index.js
@@ -0,0 +1,10 @@
+let a=2,b=3;
+console.log(a+b)// + is operator. 2,3 is operands
+
+// Type of operator we have
+
+// arthemtic operator:+,-,*,**,%,/,++,--
+// assignment operator:=,+=,-=,*=,/=,**=,%=
+// comparison operator:==,===,!=,!==,>,<,>=,<=
+// logical operatot: &&,||,!
+// ternary operator:?
