cbb2c075f3fd79a3444674fb1fa042995ed8ddb6
akash <akashnirwan26@gmail.com>
2023-09-11 13:39:27 +0530

    class9

---
+++ class9/index.js
@@ -0,0 +1,21 @@
+
+// let a=20;
+// if(a==10){
+//     console.log(a)
+// }
+// else if(a>30){
+// console.log(a)
+// }
+// else{
+//     console.log(a)
+// }
+
+// (a>10)?console.log(a):console.log(b)
+
+let a=4;
+if(a==4){
+    console.log(a)
+    if(a>3){
+        console.log(a)
+    }
+}
